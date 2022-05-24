6752c0e173a234ba54787177e6bcbbba6efc0850
eduardocaqm <eduarduo.aquino@gmail.com>
2022-05-24 18:10:59 -0300

    first commit

---
+++ src/javaclass/primeiroProgramaJava.java
@@ -0,0 +1,18 @@
+package javaclass;
+
+import javaclass.model.Gato;
+
+public class primeiroProgramaJava {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+        /*   int a = 5;
+        int b = 3;
+        System.out.println("hello word "+ (a+b));*/
+    }
+}
+
+//crtl+d duplica linha- crtl+y deleta linha
