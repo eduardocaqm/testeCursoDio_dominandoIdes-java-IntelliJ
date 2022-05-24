07414e25956e485f825103ff7833a93ae4b3f83b
eduardocaqm <eduarduo.aquino@gmail.com>
2022-05-24 19:12:35 -0300

    criando class livro

--- src/javaclass/primeiroProgramaJava.java
+++ src/javaclass/primeiroProgramaJava.java
@@ -5,14 +5,47 @@ import javaclass.model.Gato;
 public class primeiroProgramaJava {
     public static void main(String[] args) {
         Gato gato = new Gato();
-
         System.out.println(gato);
 
+        Livro livro1 = new Livro ("consegui em fim fazer isso",300);
 
-        /*   int a = 5;
-        int b = 3;
-        System.out.println("hello word "+ (a+b));*/
+
+        System.out.println (livro1);
+
     }
 }
+class Livro {
+    private String nome;
+    private Integer numPag;
 
-//crtl+d duplica linha- crtl+y deleta linha
+    public Livro(String nome, Integer numPag) {
+        this.nome = nome;
+        this.numPag = numPag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(Integer numPag) {
+        this.numPag = numPag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
+    }
+
+
+}
